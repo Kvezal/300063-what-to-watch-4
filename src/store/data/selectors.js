a2d2721c437866582ff1b6de6e00eeb547c2371b
Kvezal <muckam49@mail.ru>
2020-07-17 10:34:36 +0800

    refactor(store): reselect package installed for select creating (module7-task2)

--- src/store/data/selectors.js
+++ src/store/data/selectors.js
@@ -1,54 +1,79 @@
-import NameSpace from "../name-space.js";
+import {createSelector} from "reselect";
+
 import {GenreEnum} from "@common/enums";
+
+import NameSpace from "../name-space.js";
 
 
 const NAME_SPACE = NameSpace.DATA;
+
+const getFilms = (state) => {
+  return state[NAME_SPACE].films;
+};
 
 const getPromoFilm = (state) => {
   return state[NAME_SPACE].promoFilm;
 };
 
-const getFilteredFilmsByGenre = (state) => {
-  if (state[NAME_SPACE].genre === GenreEnum.ALL) {
-    return state[NAME_SPACE].films;
-  }
-  return state[NAME_SPACE].films
-    .filter((film) => film.genre === state[NAME_SPACE].genre);
+const getCurrentFilmId = (state) => {
+  return state[NAME_SPACE].currentFilmId;
 };
 
-const getFilmById = (state) => {
-  return state[NAME_SPACE].films
-    .find((film) => film.id === state[NAME_SPACE].currentFilmId);
-};
-
-const getLikedFilms = (state) => {
-  const currentFilm = getFilmById(state);
-  if (!currentFilm) {
-    return [];
-  }
-  return state[NAME_SPACE].films
-    .filter((film) => film.genre === currentFilm.genre)
-    .slice(0, 4);
-};
-
-const getReviews = (state) => {
-  return state[NAME_SPACE].filmReviews;
+const getCurrentGenre = (state) => {
+  return state[NAME_SPACE].genre;
 };
 
 const getFilmId = (state) => {
   return state[NAME_SPACE].currentFilmId;
 };
 
+const getReviews = (state) => {
+  return state[NAME_SPACE].filmReviews;
+};
+
 const getNotifications = (state) => {
   return state[NAME_SPACE].notifications;
 };
 
+const getFilmById = createSelector(
+    getFilms,
+    getCurrentFilmId,
+    (films, filmId) => {
+      return films.find((film) => film.id === filmId);
+    }
+);
+
+const getLikedFilms = createSelector(
+    getFilms,
+    getFilmById,
+    (films, currentFilm) => {
+      if (!currentFilm) {
+        return [];
+      }
+      return films
+        .filter((film) => film.genre === currentFilm.genre)
+        .slice(0, 4);
+    }
+);
+
+const getFilteredFilmsByGenre = createSelector(
+    getFilms,
+    getCurrentGenre,
+    (films, genre) => {
+      if (genre === GenreEnum.ALL) {
+        return films;
+      }
+      return films.filter((film) => film.genre === genre);
+    }
+);
+
 export {
+  getFilms,
   getPromoFilm,
-  getFilteredFilmsByGenre,
-  getLikedFilms,
-  getFilmById,
   getReviews,
   getFilmId,
   getNotifications,
+  getLikedFilms,
+  getFilmById,
+  getFilteredFilmsByGenre,
 };
