be6a1364139b34e6250cfaac3f0be9771fa851fd
Kvezal <muckam49@mail.ru>
2020-07-27 17:15:53 +0800

    feat(pages): my list button can change isFavorite state in Main page (module8-task2)

--- src/pages/main/main.jsx
+++ src/pages/main/main.jsx
@@ -30,7 +30,7 @@ const Main = (props) => {
     onStepReset,
     onFavoriteFilmClick,
   } = props;
-  const {id: promoFilmId, genre, releaseDate, name, picture} = promoFilm;
+  const {id: promoFilmId, genre, releaseDate, name, picture, isFavorite} = promoFilm;
 
   return <Fragment>
     <section className="movie-card">
@@ -74,9 +74,14 @@ const Main = (props) => {
                   type="button"
                   onClick={() => onFavoriteFilmClick(promoFilm)}
                 >
-                  <svg viewBox="0 0 19 20" width="19" height="20">
-                    <use xlinkHref="#add"/>
-                  </svg>
+                  {isFavorite
+                    ? <svg viewBox="0 0 18 14" width="18" height="14">
+                      <use xlinkHref="#in-list"/>
+                    </svg>
+                    : <svg viewBox="0 0 19 20" width="19" height="20">
+                      <use xlinkHref="#add"/>
+                    </svg>
+                  }
                   <span>My list</span>
                 </button>
               )}
